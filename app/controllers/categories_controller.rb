7ec2f012d72f51b8b7371b766e0ca146b67a2e23
Kate Connolly <katherine.d.connolly@gmail.com>
2018-12-06 22:49:55 +0000

    fixed category search feature, added a new category form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,31 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @category = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
   def show
+    p "this is not a thing"
     @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :category]
+    else
+      render 'new'
+    end
+  end
+
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
